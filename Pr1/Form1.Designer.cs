1eb49d63be9beebadf15ad32e9d1c5cf9bc550e1
anhansnail <36840957+anhansnail@users.noreply.github.com>
2019-05-14 21:13:18 +0700

    FlieSystemWatch1 show thong bao khi co thay doi trong thu muc tai duong dan D:\demo_watch

--- Pr1/Form1.Designer.cs
+++ Pr1/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.IncludeSubdirectories = true;
+            this.fileSystemWatcher1.Path = "D:\\demo_watch";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            this.fileSystemWatcher1.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher1_Renamed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
     }
 }
 
--- Pr1/Form1.cs
+++ Pr1/Form1.cs
@@ -16,5 +16,29 @@ namespace Pr1
         {
             InitializeComponent();
         }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            MessageBox.Show(string.Format("File_Changed {0} {1}", e.FullPath, e.Name));
+        }
+
+        private void fileSystemWatcher1_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            MessageBox.Show(string.Format("File_Created {0} {1}", e.FullPath, e.Name));
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, System.IO.FileSystemEventArgs e)
+        {
+            MessageBox.Show(string.Format("File_Deleted {0} {1}", e.FullPath, e.Name));
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, System.IO.RenamedEventArgs e)
+        {
+            //đây là even sửa tên
+            //nếu sửa tên file/thư mục trong path đã đặt thì sẽ hiện lên 1 cái messagebox thông báo
+            //tương tự với mấy cái trên
+
+            MessageBox.Show(string.Format("File_Renamed {0} {1}", e.FullPath, e.Name));
+        }
     }
 }
